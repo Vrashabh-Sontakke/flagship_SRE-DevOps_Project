ef0ff3d58f32a15c2007f7c2e506f587f4e31362
Vrashabh <86019029+Vrashabh-Sontakke@users.noreply.github.com>
2023-02-07 05:52:27 +0530

    added `eks-managed-node-groups` and `tags`

--- eks_cluster.tf
+++ eks_cluster.tf
@@ -15,4 +15,35 @@ module "eks_cluster" {
   vpc_id     = aws_vpc.main.id
 
   enable_irsa = true
+
+   eks_managed_node_group_defaults = {
+    ami_type       = var.group_ami_type
+    instance_types = var.group_instance_type
+
+    attach_cluster_primary_security_group = false
+    vpc_security_group_ids                = [aws_security_group.allow-web-traffic.id]
+  }
+
+  eks_managed_node_groups = {
+    jenkins = {
+      ami_type       = var.group_ami_type
+      platform       = var.group_platform_type
+      instance_types = [var.group_instance_type[1]]
+      desired_size   = var.group_desired_size
+      min_size       = var.group_min_size
+      max_size       = var.group_max_size
+      capacity_type  = var.group_capacity_type
+      labels = {
+        Name = "jenkins-${var.name}-${data.aws_region.current.name}"
+      }
+      tags = merge(var.tags, {
+        "Name" = "jenkins-${var.name}-${data.aws_region.current.name}"
+      })
+    }
+  
+  }
+
+  tags = merge(var.tags, {
+    "Name" = "eks-cluster-${var.name}-${data.aws_region.current.name}"
+  })
 }
